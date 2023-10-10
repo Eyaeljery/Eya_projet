d5a7c282a651a4b874b0a43d0f4d6fdeb7c96aba
Bouachiri Ahmed <99204293+Bouhmid23@users.noreply.github.com>
2023-10-10 10:29:23 +0100

    Create commit3.js

---
+++ tests/commit3.js
@@ -0,0 +1,7 @@
+function retourneUneChaine() {
+  return "your Skin and bones, turning into something beautiful";
+}
+
+var maChaine = retourneUneChaine();
+if typeof maChaine == String{
+  console.log(maChaine);}
